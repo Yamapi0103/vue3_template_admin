dda5ffd38461113e1a1536a46c257e02c53c915d
yamapi0103 <yamapi0103@gmail.com>
2025-01-12 18:36:38 +0800

    add element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhTW from 'element-plus/es/locale/lang/zh-tw'
 
-createApp(App).mount('#app')
+const app = createApp(App).use(ElementPlus, {
+  locale: zhTW,
+})
+
+app.mount('#app')
